543db540956f52667bb0edc1413f0e03dcf161d7
Tarun Lahrod <lahrod.tarun46@gmail.com>
2018-11-22 23:44:36 +0530

    optimised and documented

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -8,6 +8,7 @@ Space complexity: O(1)
 #include<iostream>
 using namespace std;
 
+//printMe() is a function to print the array which is passed to it as parameter
 void printMe(int a[], int size)
 {
 	for(int i=0; i<size; i++)
@@ -15,10 +16,21 @@ void printMe(int a[], int size)
 	cout<<"\n";
 }
 
+//bubble sort function
 
 void bubbleSort(int a[], int size)
 {
 	int flag;
+	/* flag is a variable we used to make our code a bit more optimised and efficient.
+	   The code would have been iterated for size-1 times (which generally happens in a basic bubble sort)
+	   in absence of this variable flag. In bubble sort even if the given array has been 
+	   sorted even then the code keep iterating for size-1 times (which makes it redundant). 
+	   So for optimisation we use this concept of flag. What it does is that it is initialised
+	   as 0 (zero) and is upgraded to 1 whenever a swapping between the elements if performed and then
+	   made equal to zero after every pass. Say during any iteration in between if the flag didn't changed, which implies
+	   that our array is now sorted, hence we break out of the iterator.
+	   This concept optimised our code.
+	 */
 
 	for(int i=0;i<size-1;i++)
 	{
