dae56adb79f48e8dc35013f8191b56a1135f05e4
Tarun Lahrod <lahrod.tarun46@gmail.com>
2018-11-23 23:38:31 +0530

    still has bugs

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -1,61 +1,39 @@
+/*					Sieve of Eratosthenes
+	This is a method to find all the prime numbers from 0 to any number.
+	It is one of the most efficient method.
+	If anyone can make this code better, please make it. Thanks.
+*/
+
 #include<iostream>
 using namespace std;
 
-void primeClassical(long a,long b)		//	finding primes between a and b
-{
-	for(long j=a;j<=b;j++)
-	{
-		if(j==1)
-			continue;
-		int flag=0;
-		for(long i=2;i<=j/2;i++)
-		{
-			if(j%i == 0)
-			{
-				flag=1; 
-				break;
-			}
-		}
-		if(!flag)
-			cout<<j<<endl;
-	}
-}
-
-
-void primeBySieveofEratosthenes(long a, long b)
+void primeBySieveofEratosthenes(long a)
 {
 	// first we'll find primes between zero and a number 'a'
 	//declaring a boolean array of required size
 	bool numArray[a];
 	for(int i = 0 ; i<a ;i++)
-		numArray[i] = true ;
+		numArray[i] = true ;  	// setting all values true initially
+			      		// true means it is a prime number, we are assuming that all are prime numbers
+					// we will remove them in the next loop (by making their value = false.
 
 	for(int i=2;i<a;i++)
 	{
 		if(numArray[i])			//if numArray[i] is true
 		{
-			for(int n = 2; numArray[i]*n < a ; n++)   // n is the multiple
-				numArray[numArray[i]*n] = false;				  //removing all the mutliples of numArray[i]
+			for(int n = 2; numArray[i]*n < a ; n++)   		// n is the multiple
+				numArray[numArray[i]*n] = false;		//removing all the mutliples of numArray[i]
 		}
 	}
-
-	for(int i = 2;i<a;i++)
-		if(numArray[i])
-			cout<<i<<" ";
+	
+	//for printing the primes (or the "true" values)
+	for(int i = 2 ; i < a ; i++)
+		if(numArray[i])			// if numArray is true, then print it's index (which is nothing but the number itself
+			cout<<i<<" ";		// else do nothing just update and keep iterating.
 }
 
 int main()
 {
-	long a=100,b=1000000;
-	int t=1;
-	primeBySieveofEratosthenes(a,b);
-	// //making the test case to follow and perform the given task
-	// cin>>t;
-	// while(t--)
-	// {
-	// 	cin>>a>>b;
-	// 	fun(a,b);
-	// 	cout<<endl;
-	// }
-	//cout<<"hi chal rha";
+	long a = 100;
+	primeBySieveofEratosthenes(a);
 }
