9c08869e9d5b5db39b25977165c4548b675d72df
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-06-18 02:23:30 +0530

    Update string to int recursion.cpp

--- Recursion/string to int recursion.cpp
+++ Recursion/string to int recursion.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
+#include<cstring>
 using namespace std;
 
 int stringToInt(char *a, int n){
 	if(n==0)
 		return 0;
-	return stringToInt(a, n-1)*10 + (a[n-1]-48); 		// -48 because the ASCII value of 0 is 48
+	return stringToInt(a, n-1)*10 + (a[n-1] - '0'); 		// '0' = -48 because the ASCII value of 0 is 48
 }
 
 int main(){
 	char a[] = "1234";
-	int n = sizeof(a)/sizeof(char) - 1;
+	int n = strlen(a);
 	int num = stringToInt(a, n);
 	cout<<num<<endl;
 }
