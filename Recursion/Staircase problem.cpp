5870e27d2869119c70fe04023a38b28438ca8fdb
Tarun Lahrod <lahrod.tarun46@gmail.com>
2019-07-04 08:59:39 +0530

    Update Staircase problem.cpp

--- Recursion/Staircase problem.cpp
+++ Recursion/Staircase problem.cpp
@@ -1,3 +1,14 @@
+/*
+
+Problem:
+Count ways to reach the n’th stair
+There are n stairs, a person standing at the bottom wants to reach the top. 
+The person can climb either 1 stair or 2 stairs at a time. 
+Count the number of ways, the person can reach the top.
+
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
